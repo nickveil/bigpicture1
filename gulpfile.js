91f3c0f9e364709b3769a2a5b142ee65ef165adc
nickveil <31191433+nickveil@users.noreply.github.com>
2017-09-22 15:06:20 -0400

    gulpfile correction

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browesr-sync').create();
+var browserSync = require('browser-sync').create();
 
 
 gulp.task('default', function() {
